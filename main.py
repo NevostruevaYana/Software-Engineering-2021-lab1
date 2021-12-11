5e554ee5c26132ec159e9fa8435fb82f09b70e4e
NevostruevaYana <nevostrueva.yaa@gmail.com>
2021-12-11 18:47:13 +0300

    added parsing of arguments and converting the input number of dollars into euros

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import argparse
 from converter import USDEURConverter
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("python main.py", description="USD EUR Converter")
+    parser.add_argument("-v", "--value", help="the value to be converted from dollar to euro",
+                        type=str, default="1")
+    args = parser.parse_args()
+
     currency = USDEURConverter.check_currency().replace(",", ".")
+    float_currency = float(currency)
+    answer = USDEURConverter.calculate_value(float_currency, float(args.value))
     print("Current USD -> EUR exchange rate: " + currency)
+    print(args.value + " USD = " + str(answer) + " EUR")
