54b0f826062bd9ebaa478bb3aab9e792140fd700
NevostruevaYana <nevostrueva.yaa@gmail.com>
2021-12-11 18:49:21 +0300

    #1 added new arguments (currencies from and to)

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ from converter import USDEURConverter
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("python main.py", description="USD EUR Converter")
-    parser.add_argument("-v", "--value", help="the value to be converted from dollar to euro",
+    parser.add_argument("-v", "--value", help="the value to be converted",
                         type=str, default="1")
+    parser.add_argument("-f", "--ffrom", help="from currency",
+                        type=str, default="USD")
+    parser.add_argument("-t", "--to", help="to currency",
+                        type=str, default="EUR")
     args = parser.parse_args()
+
+    from_currency = args.ffrom
+    to_currency = args.to
+    value = float(args.value)
 
     currency = USDEURConverter.check_currency().replace(",", ".")
     float_currency = float(currency)
